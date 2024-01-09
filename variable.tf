e509de410a61444ee7c7a4a4b93e6ec0a91f92b8
achikam <achchusnulchikam2@gmail.com>
2024-01-09 07:46:07 +0700

    update  variable

--- variable.tf
+++ variable.tf
@@ -1,3 +1,12 @@
 variable "username" {
   default = "admin-uat"
 }
+variable "password" {
+  default = "Redhat123!@#"
+}
+variable "project" {
+  default = "uat-project"
+}
+variable "doamin" {
+  default = "uat"
+}
