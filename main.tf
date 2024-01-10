f0b069306775a0df174724d3042d828b48d989ed
achikam <achchusnulchikam2@gmail.com>
2024-01-10 14:45:30 +0700

    update main

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 locals {
   servers = {
-    "serverA" = { floatingip = "125.213.130.182", volume= "vol_serverA" },
-    "serverB" = { floatingip = "125.213.130.183", volume= "vol_serverB" },
+    "serverA" = { privateip = "10.20.20.11", floatingip = "125.213.130.182", volume= "vol_serverA" },
+    "serverB" = { privateip = "10.20.20.12", floatingip = "125.213.130.183", volume= "vol_serverB" },
+    "serverC" = { privateip = "10.20.20.13", floatingip = "125.213.130.184", volume= "vol_serverC" },
   }
 }
 
@@ -26,20 +27,76 @@ resource "openstack_compute_keypair_v2" "tf-keypair" {
   name = "terra-keypair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGoFEgRQDiK3/jg7yptJ5TZi4WXlbvCa9w0cAUg4Mc4omomzXPF3XstpBC2xCSWRpsIhWusLr20p3M4tDkb87zxdmGZtJPljNn96KkX17lE0OJxHX4sMsCs1eHOsnhc5jcpnR43ATA/uOgVYEatDaIkaXgMQZeo4/+EYNcCoNMpk3CZAr8158iINWTRQFl+Ya814Bqa6+h3NEgiyN7R+dDdz7LFh2U05E9v4HodFpERx6waLfGjhdjXMVhZDLyDdulO+ETofQUG2uATz2Ocv8Cw7VEfG8QsJ8J0u2tdvCXVcqQ5gB7Xg34+/mn+j/bfLlDgu/C1VlqU05uELv50Cel ACC@ACC-MSI-LAPTOP"
 }
-resource "openstack_compute_secgroup_v2" "tf-secgroup1" {
-  name        = "tf-secgroup"
-  description = "a security group by terraform"
-  
+resource "openstack_compute_secgroup_v2" "m-secgroup" {
+  name        = "m-secgroup"
+  description = "Security Group for Kubernetes Control Plane"
   rule {
     from_port   = 22
     to_port     = 22
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
-  
   rule {
     from_port   = 80
     to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 6443
+    to_port     = 6443
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 10250
+    to_port     = 10250
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 10257
+    to_port     = 10257
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 10259
+    to_port     = 10259
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 2379
+    to_port     = 2380
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+resource "openstack_compute_secgroup_v2" "w-secgroup" {
+  name        = "w-secgroup"
+  description = "Security Group for Kubernetes Worker"
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 10250
+    to_port     = 10250
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 30000
+    to_port     = 32767
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
@@ -55,6 +112,18 @@ resource "openstack_networking_subnet_v2" "tf-subnet1" {
   cidr       = "10.20.20.0/24"
   ip_version = 4
 }
+
+resource "openstack_networking_port_v2" "tf-port" {
+  for_each       = local.servers
+  name           = "private-ip"
+  admin_state_up = "true"
+  network_id     = openstack_networking_network_v2.tf-network1.id
+  fixed_ip {
+    subnet_id   = openstack_networking_subnet_v2.tf-subnet1.id
+    ip_address  = each.value.privateip
+  }
+}
+
 resource "openstack_networking_router_v2" "tf-router" {
   name                = "terra-router"
   admin_state_up      = true
@@ -65,24 +134,30 @@ resource "openstack_networking_router_interface_v2" "tf-router_interface" {
   subnet_id = openstack_networking_subnet_v2.tf-subnet1.id
 }
 
-# resource "openstack_networking_floatingip_v2" "tf-fip1" {
+# resource "openstack_networking_floatingip_v2" "tf-fip" {
 #   pool = "Public"
 #   depends_on = [openstack_networking_subnet_v2.tf-subnet1]
 # }
 
-resource "openstack_networking_floatingip_v2" "tf-fip1" {
+resource "openstack_networking_floatingip_v2" "tf-fip" {
   for_each = local.servers
   pool = "Public"
   address = each.value.floatingip
-#   address = "125.213.130.186"
 }
 
+resource "openstack_compute_servergroup_v2" "tf-servergroup" {
+  name     = "m-servergroup"
+  policies = ["anti-affinity"]
+}
 resource "openstack_compute_instance_v2" "tf-vm1" {
   for_each = local.servers
   name      = each.key
   flavor_id       = openstack_compute_flavor_v2.tf_flavor1.id
   key_pair        = openstack_compute_keypair_v2.tf-keypair.name
-  security_groups = [openstack_compute_secgroup_v2.tf-secgroup1.name]
+  security_groups = [openstack_compute_secgroup_v2.m-secgroup.name]
+  scheduler_hints {
+    group = openstack_compute_servergroup_v2.tf-servergroup.id
+  }
   block_device {
     # uuid                  = openstack_blockstorage_volume_v3.tf-vol1.id
     uuid                  = openstack_blockstorage_volume_v3.tf-vol1[each.key].id
@@ -93,6 +168,7 @@ resource "openstack_compute_instance_v2" "tf-vm1" {
   }
   network {
     uuid = openstack_networking_network_v2.tf-network1.id
+    port = openstack_networking_port_v2.tf-port[each.key].id
   }
   user_data = file("user_data.yaml")
 }
@@ -102,29 +178,31 @@ resource "openstack_compute_instance_v2" "tf-vm1" {
 #   volume_id   = openstack_blockstorage_volume_v2.tf-vol1.id
 # }
 
-resource "openstack_compute_floatingip_associate_v2" "tf-fip1" {
-  for_each = local.servers
-  floating_ip = openstack_networking_floatingip_v2.tf-fip1[each.key].address
-  instance_id = openstack_compute_instance_v2.tf-vm1[each.key].id
-}
+# resource "openstack_compute_floatingip_associate_v2" "tf-fip" {
+#   for_each = local.servers
+#   floating_ip = openstack_networking_floatingip_v2.tf-fip[each.key].address
+#   instance_id = openstack_compute_instance_v2.tf-vm1[each.key].id
+# }
 
-resource "null_resource" "example" {
-  for_each = local.servers
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
-    # host     = openstack_networking_floatingip_v2.tf-fip1["each.value.floatingip"]
-    host     = openstack_networking_floatingip_v2.tf-fip1[each.key].address
-  }
-  provisioner "remote-exec" {
-    script  = "script/kube.sh"
-    on_failure = continue
-  }
-}
+# resource "null_resource" "example" {
+#   for_each = local.servers
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file("~/.ssh/id_rsa")
+#     host     = openstack_networking_floatingip_v2.tf-fip[each.key].address
+#     timeout  = "60s"
+#   }
+#   provisioner "remote-exec" {
+#     script  = "script/kube.sh"
+#     on_failure = continue
+#   }
+# }
 
 output "floating_ip_Instance" {
-#   value = openstack_networking_floatingip_v2.tf-fip1.address
-  value = {for fip in openstack_networking_floatingip_v2.tf-fip1: fip.id => fip.address }
+#   value = openstack_networking_floatingip_v2.tf-fip["each.value.floatingip"]
+  value = { for fip in openstack_networking_floatingip_v2.tf-fip: fip.id => fip.address }
 }
-
+output "private_ip" {
+  value = { for private in openstack_networking_port_v2.tf-port: private.id => private.fixed_ip[0].ip_address }
+}
