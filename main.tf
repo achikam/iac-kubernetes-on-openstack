ff295fdc811467833faf8f1bf5aa6b4d9f478846
achikam <achchusnulchikam2@gmail.com>
2024-01-10 15:03:22 +0700

    nice

--- main.tf
+++ main.tf
@@ -159,7 +159,6 @@ resource "openstack_compute_instance_v2" "tf-vm1" {
     group = openstack_compute_servergroup_v2.tf-servergroup.id
   }
   block_device {
-    # uuid                  = openstack_blockstorage_volume_v3.tf-vol1.id
     uuid                  = openstack_blockstorage_volume_v3.tf-vol1[each.key].id
     source_type           = "volume"
     boot_index            = 0
