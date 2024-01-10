88838c2013fccb9ef1598274849319c40b6adea1
achikam <achchusnulchikam2@gmail.com>
2024-01-10 10:34:10 +0700

    commit

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+locals {
+  servers = {
+    "serverA" = { floatingip = "125.213.130.182", volume= "vol_serverA" },
+    "serverB" = { floatingip = "125.213.130.183", volume= "vol_serverB" },
+  }
+}
+
 resource "openstack_compute_flavor_v2" "tf_flavor1" {
   name      = "tf-flavor"
   ram       = "1024"
@@ -8,8 +15,10 @@ resource "openstack_compute_flavor_v2" "tf_flavor1" {
     "aggregate_instance_extra_specs:processor" = "intel",
   }
 }
+
 resource "openstack_blockstorage_volume_v3" "tf-vol1" {
-  name = "myvol"
+  for_each = local.servers
+  name = each.value.volume
   size = 6
   image_id = "56383473-17de-4baf-8b9b-3711bebe5d3d"
 }
@@ -20,37 +29,22 @@ resource "openstack_compute_keypair_v2" "tf-keypair" {
 resource "openstack_compute_secgroup_v2" "tf-secgroup1" {
   name        = "tf-secgroup"
   description = "a security group by terraform"
-rule {
+  
+  rule {
     from_port   = 22
     to_port     = 22
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
+  
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
 }
 
-# resource "openstack_networking_network_v2" "tf-publicnet" {
-#   name           = "public"
-#   segments {
-#     network_type   = "flat"
-#     physical_network = "datacentre"
-#   }
-#   shared         = "true"
-#   admin_state_up = "true"
-#   external       = "true"
-# }
-# resource "openstack_networking_subnet_v2" "tf-publicsubnet" {
-#   name       = "public-sub"
-#   network_id = openstack_networking_network_v2.tf-publicnet.id
-#   cidr       = "192.168.240.0/24"
-#   ip_version = 4
-#   enable_dhcp = "true"
-#   gateway_ip  = "192.168.240.250"
-#   allocation_pool {
-#     end   = "192.168.240.100"
-#     start = "192.168.240.60"
-#   }
-#   dns_nameservers = ["192.168.240.250","8.8.8.8"]
-# }
 resource "openstack_networking_network_v2" "tf-network1" {
   name           = "tf-net"
   admin_state_up = "true"
@@ -76,14 +70,22 @@ resource "openstack_networking_router_interface_v2" "tf-router_interface" {
 #   depends_on = [openstack_networking_subnet_v2.tf-subnet1]
 # }
 
+resource "openstack_networking_floatingip_v2" "tf-fip1" {
+  for_each = local.servers
+  pool = "Public"
+  address = each.value.floatingip
+#   address = "125.213.130.186"
+}
+
 resource "openstack_compute_instance_v2" "tf-vm1" {
-  name          = "tf-vm"
-#   image_name    = "ubuntu20"
+  for_each = local.servers
+  name      = each.key
   flavor_id       = openstack_compute_flavor_v2.tf_flavor1.id
   key_pair        = openstack_compute_keypair_v2.tf-keypair.name
   security_groups = [openstack_compute_secgroup_v2.tf-secgroup1.name]
   block_device {
-    uuid                  = openstack_blockstorage_volume_v3.tf-vol1.id
+    # uuid                  = openstack_blockstorage_volume_v3.tf-vol1.id
+    uuid                  = openstack_blockstorage_volume_v3.tf-vol1[each.key].id
     source_type           = "volume"
     boot_index            = 0
     destination_type      = "volume"
@@ -101,18 +103,29 @@ resource "openstack_compute_instance_v2" "tf-vm1" {
 # }
 
 resource "openstack_compute_floatingip_associate_v2" "tf-fip1" {
-#   floating_ip = openstack_networking_floatingip_v2.tf-fip1.address
-  floating_ip = "125.213.130.186"
-  instance_id = openstack_compute_instance_v2.tf-vm1.id
+  for_each = local.servers
+  floating_ip = openstack_networking_floatingip_v2.tf-fip1[each.key].address
+  instance_id = openstack_compute_instance_v2.tf-vm1[each.key].id
 }
 
 resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo Hello World from local-exec terraform >> /tmp/file-from-terraform.txt "
+  for_each = local.servers
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    # password = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    # host     = openstack_networking_floatingip_v2.tf-fip1["each.value.floatingip"]
+    host     = openstack_networking_floatingip_v2.tf-fip1[each.key].address
+  }
+  provisioner "remote-exec" {
+    script  = "script/tes.sh"
+    on_failure = continue
   }
 }
 
-# output "floating_ip_Instance" {
+output "floating_ip_Instance" {
 #   value = openstack_networking_floatingip_v2.tf-fip1.address
-# }
+  value = {for fip in openstack_networking_floatingip_v2.tf-fip1: fip.id => fip.address }
+}
 
