55fcefc114849fa64c51cc47ed81a60d9d72d02c
achikam <achchusnulchikam2@gmail.com>
2024-01-12 15:03:57 +0700

    update

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
 locals {
   masters = {
     "master" = { privateip = "10.20.20.11", floatingip = "125.213.130.182", volume_name = "vol_master", volume_size = "10" },
-    # "worker1" = { privateip = "10.20.20.12", floatingip = "125.213.130.183", volume_name = "vol_worker1", volume_size = "10" },
-    # "worker2" = { privateip = "10.20.20.13", floatingip = "125.213.130.184", volume_name = "vol_worker2", volume_size = "10" },
   }
-}
-
-locals {
   workers = {
-    # "master" = { privateip = "10.20.20.11", floatingip = "125.213.130.182", volume_name = "vol_master", volume_size = "10" },
     "worker1" = { privateip = "10.20.20.12", floatingip = "125.213.130.183", volume_name = "vol_worker1", volume_size = "10" },
     "worker2" = { privateip = "10.20.20.13", floatingip = "125.213.130.184", volume_name = "vol_worker2", volume_size = "10" },
   }
@@ -25,18 +19,23 @@ resource "openstack_compute_flavor_v2" "tf_flavor1" {
   }
 }
 
-resource "openstack_blockstorage_volume_v3" "m-vol" {
+variable "image_id" {
+  description = "ID of the image to be used for instances - Ubuntu 22.04"
+  default     = "56383473-17de-4baf-8b9b-3711bebe5d3d"
+}
+
+resource "openstack_blockstorage_volume_v3" "master-vol" {
   for_each = local.masters
   name = each.value.volume_name
   size = each.value.volume_size
-  image_id = "56383473-17de-4baf-8b9b-3711bebe5d3d"
+  image_id = var.image_id
 }
 
-resource "openstack_blockstorage_volume_v3" "w-vol" {
+resource "openstack_blockstorage_volume_v3" "worker-vol" {
   for_each = local.workers
   name = each.value.volume_name
   size = each.value.volume_size
-  image_id = "56383473-17de-4baf-8b9b-3711bebe5d3d"
+  image_id = var.image_id
 }
 
 resource "openstack_compute_keypair_v2" "tf-keypair" {
@@ -44,8 +43,8 @@ resource "openstack_compute_keypair_v2" "tf-keypair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGoFEgRQDiK3/jg7yptJ5TZi4WXlbvCa9w0cAUg4Mc4omomzXPF3XstpBC2xCSWRpsIhWusLr20p3M4tDkb87zxdmGZtJPljNn96KkX17lE0OJxHX4sMsCs1eHOsnhc5jcpnR43ATA/uOgVYEatDaIkaXgMQZeo4/+EYNcCoNMpk3CZAr8158iINWTRQFl+Ya814Bqa6+h3NEgiyN7R+dDdz7LFh2U05E9v4HodFpERx6waLfGjhdjXMVhZDLyDdulO+ETofQUG2uATz2Ocv8Cw7VEfG8QsJ8J0u2tdvCXVcqQ5gB7Xg34+/mn+j/bfLlDgu/C1VlqU05uELv50Cel ACC@ACC-MSI-LAPTOP"
 }
 
-resource "openstack_compute_secgroup_v2" "m-secgroup" {
-  name        = "m-secgroup"
+resource "openstack_compute_secgroup_v2" "master-secgroup" {
+  name        = "master-secgroup"
   description = "Security Group for Kubernetes Control Plane"
   rule {
     from_port   = 22
@@ -90,8 +89,8 @@ resource "openstack_compute_secgroup_v2" "m-secgroup" {
     cidr        = "0.0.0.0/0"
   }
 }
-resource "openstack_compute_secgroup_v2" "w-secgroup" {
-  name        = "w-secgroup"
+resource "openstack_compute_secgroup_v2" "worker-secgroup" {
+  name        = "worker-secgroup"
   description = "Security Group for Kubernetes Worker"
   rule {
     from_port   = 22
@@ -130,7 +129,7 @@ resource "openstack_networking_subnet_v2" "tf-subnet1" {
   ip_version = 4
 }
 
-resource "openstack_networking_port_v2" "m-port" {
+resource "openstack_networking_port_v2" "master-port" {
   for_each       = local.masters
   name           = "port-master-${each.value.privateip}"
   admin_state_up = "true"
@@ -141,7 +140,16 @@ resource "openstack_networking_port_v2" "m-port" {
   }
 }
 
-resource "openstack_networking_port_v2" "w-port" {
+resource "openstack_networking_port_secgroup_associate_v2" "master-port-associate" {
+  for_each       = local.masters
+  port_id = openstack_networking_port_v2.master-port[each.key].id
+  enforce = "true"
+  security_group_ids = [
+    openstack_compute_secgroup_v2.master-secgroup.id,
+  ]
+}
+
+resource "openstack_networking_port_v2" "worker-port" {
   for_each       = local.workers
   name           = "port-worker-${each.value.privateip}"
   admin_state_up = "true"
@@ -150,6 +158,15 @@ resource "openstack_networking_port_v2" "w-port" {
     subnet_id   = openstack_networking_subnet_v2.tf-subnet1.id
     ip_address  = each.value.privateip
   }
+}
+
+resource "openstack_networking_port_secgroup_associate_v2" "worker-port-associate" {
+  for_each       = local.workers
+  port_id = openstack_networking_port_v2.worker-port[each.key].id
+  enforce = "true"
+  security_group_ids = [
+    openstack_compute_secgroup_v2.worker-secgroup.id,
+  ]
 }
 
 resource "openstack_networking_router_v2" "tf-router" {
@@ -162,34 +179,35 @@ resource "openstack_networking_router_interface_v2" "tf-router_interface" {
   subnet_id = openstack_networking_subnet_v2.tf-subnet1.id
 }
 
-resource "openstack_networking_floatingip_v2" "m-fip" {
+resource "openstack_networking_floatingip_v2" "master-fip" {
   for_each = local.masters
   pool = "Public"
   address = each.value.floatingip
 }
-resource "openstack_networking_floatingip_v2" "w-fip" {
+resource "openstack_networking_floatingip_v2" "worker-fip" {
   for_each = local.workers
   pool = "Public"
   address = each.value.floatingip
 }
 
 # MASTER VM
-resource "openstack_compute_servergroup_v2" "tf-servergroup" {
-  name     = "m-servergroup"
+resource "openstack_compute_servergroup_v2" "master-servergroup" {
+  name     = "master-servergroup"
   policies = ["anti-affinity"]
 }
 
 resource "openstack_compute_instance_v2" "master-vm" {
+  depends_on = [ openstack_networking_port_v2.master-port, openstack_compute_secgroup_v2.master-secgroup ]
   for_each = local.masters
   name      = each.key
   flavor_id = openstack_compute_flavor_v2.tf_flavor1.id
   key_pair  = openstack_compute_keypair_v2.tf-keypair.name
-  security_groups = [openstack_compute_secgroup_v2.w-secgroup.id]
+  # security_groups = [openstack_compute_secgroup_v2.master-secgroup.id]
   scheduler_hints {
-    group = openstack_compute_servergroup_v2.tf-servergroup.id
+    group = openstack_compute_servergroup_v2.master-servergroup.id
   }
   block_device {
-    uuid                  = openstack_blockstorage_volume_v3.m-vol[each.key].id
+    uuid                  = openstack_blockstorage_volume_v3.master-vol[each.key].id
     source_type           = "volume"
     boot_index            = 0
     destination_type      = "volume"
@@ -197,28 +215,29 @@ resource "openstack_compute_instance_v2" "master-vm" {
   }
   network {
     uuid = openstack_networking_network_v2.tf-network1.id
-    port = openstack_networking_port_v2.m-port[each.key].id
+    port = openstack_networking_port_v2.master-port[each.key].id
   }
   user_data = file("user_data.yaml")
 }
 
 # WORKER VM
-resource "openstack_compute_servergroup_v2" "w-servergroup" {
-  name     = "w-servergroup"
+resource "openstack_compute_servergroup_v2" "worker-servergroup" {
+  name     = "worker-servergroup"
   policies = ["anti-affinity"]
 }
 
 resource "openstack_compute_instance_v2" "worker-vm" {
+  depends_on = [ openstack_networking_port_v2.worker-port, openstack_compute_secgroup_v2.worker-secgroup ]
   for_each = local.workers
   name      = each.key
   flavor_id = openstack_compute_flavor_v2.tf_flavor1.id
   key_pair  = openstack_compute_keypair_v2.tf-keypair.name
-  security_groups = [openstack_compute_secgroup_v2.w-secgroup.id]
+  # security_groups = [openstack_compute_secgroup_v2.worker-secgroup.id]
   scheduler_hints {
-    group = openstack_compute_servergroup_v2.w-servergroup.id
+    group = openstack_compute_servergroup_v2.worker-servergroup.id
   }
   block_device {
-    uuid                  = openstack_blockstorage_volume_v3.w-vol[each.key].id
+    uuid                  = openstack_blockstorage_volume_v3.worker-vol[each.key].id
     source_type           = "volume"
     boot_index            = 0
     destination_type      = "volume"
@@ -226,19 +245,19 @@ resource "openstack_compute_instance_v2" "worker-vm" {
   }
   network {
     uuid = openstack_networking_network_v2.tf-network1.id
-    port = openstack_networking_port_v2.w-port[each.key].id
+    port = openstack_networking_port_v2.worker-port[each.key].id
   }
   user_data = file("user_data.yaml")
 }
 
 # resource "openstack_compute_volume_attach_v2" "attached" {
 #   instance_id = openstack_compute_instance_v2.master-vm.id
-#   volume_id   = openstack_blockstorage_volume_v2.m-vol.id
+#   volume_id   = openstack_blockstorage_volume_v2.master-vol.id
 # }
 
-resource "openstack_compute_floatingip_associate_v2" "m-fip" {
+resource "openstack_compute_floatingip_associate_v2" "master-fip" {
   for_each = local.masters
-  floating_ip = openstack_networking_floatingip_v2.m-fip[each.key].address
+  floating_ip = openstack_networking_floatingip_v2.master-fip[each.key].address
   instance_id = openstack_compute_instance_v2.master-vm[each.key].id
 }
 resource "null_resource" "for-master" {
@@ -247,8 +266,7 @@ resource "null_resource" "for-master" {
     type     = "ssh"
     user     = "ubuntu"
     private_key = file(var.private_key_path)
-    # private_key = file("/home/achikam/.ssh/terra-keypair")
-    host     = openstack_networking_floatingip_v2.m-fip[each.key].address
+    host     = openstack_networking_floatingip_v2.master-fip[each.key].address
     # timeout  = "60s"
   }
   provisioner "remote-exec" {
@@ -257,9 +275,9 @@ resource "null_resource" "for-master" {
   }
 }
 
-resource "openstack_compute_floatingip_associate_v2" "w-fip" {
+resource "openstack_compute_floatingip_associate_v2" "worker-fip" {
   for_each = local.workers
-  floating_ip = openstack_networking_floatingip_v2.w-fip[each.key].address
+  floating_ip = openstack_networking_floatingip_v2.worker-fip[each.key].address
   instance_id = openstack_compute_instance_v2.worker-vm[each.key].id
 }
 resource "null_resource" "for-worker" {
@@ -267,10 +285,8 @@ resource "null_resource" "for-worker" {
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    # private_key = file("~/.ssh/id_rsa")
     private_key = file(var.private_key_path)
-    host     = openstack_networking_floatingip_v2.w-fip[each.key].address
-    # timeout  = "60s"
+    host     = openstack_networking_floatingip_v2.worker-fip[each.key].address
   }
   provisioner "remote-exec" {
     script  = "script/kube.sh"
@@ -284,7 +300,6 @@ resource "null_resource" "kubeadm-bootstrap" {
     type     = "ssh"
     user     = "ubuntu"
     private_key = file(var.private_key_path)
-    # private_key = file("/home/achikam/.ssh/terra-keypair")
     host     = local.masters["master"].floatingip
     timeout  = "600s"
   }
@@ -320,8 +335,11 @@ resource "null_resource" "copy-join-command" {
     host     = local.masters["master"].floatingip
   }
   provisioner "remote-exec" {
-    script  = "script/kubeadm-join-worker.sh"
-    on_failure = continue
+    inline = [ 
+      "sudo chmod 600 ~/.ssh/id_rsa",
+      "scp -o StrictHostKeyChecking=no join-worker.sh ubuntu@${each.value.privateip}:/tmp/",
+      "ssh -o StrictHostKeyChecking=no ubuntu@${each.value.privateip} 'sudo bash /tmp/join-worker.sh'",
+    ]
   }
 }
 
@@ -339,16 +357,15 @@ resource "null_resource" "kubeadm-complete" {
   }
 }
 
-
-output "floating_ip_Master" {
-  value = { for fip in openstack_networking_floatingip_v2.m-fip: fip.id => fip.address }
-}
-output "floating_ip_Worker" {
-  value = { for fip in openstack_networking_floatingip_v2.w-fip: fip.id => fip.address }
-}
-output "private_ip_Master" {
-  value = { for private in openstack_networking_port_v2.m-port: private.id => private.fixed_ip[0].ip_address }
-}
-output "private_ip_Worker" {
-  value = { for private in openstack_networking_port_v2.w-port: private.id => private.fixed_ip[0].ip_address }
-}
+# output "floating_ip_Master" {
+#   value = { for fip in openstack_networking_floatingip_v2.master-fip: fip.id => fip.address }
+# }
+# output "floating_ip_Worker" {
+#   value = { for fip in openstack_networking_floatingip_v2.worker-fip: fip.id => fip.address }
+# }
+# output "private_ip_Master" {
+#   value = { for private in openstack_networking_port_v2.master-port: private.id => private.fixed_ip[0].ip_address }
+# }
+# output "private_ip_Worker" {
+#   value = { for private in openstack_networking_port_v2.worker-port: private.id => private.fixed_ip[0].ip_address }
+# }
